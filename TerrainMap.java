5a5ff02695c570b64308a8bbe22db5b7b8e1222b
RohanGaddam <129432188+RohanGaddam@users.noreply.github.com>
2024-08-05 17:43:23 -0400

    Update TerrainMap.java

--- TerrainMap.java
+++ TerrainMap.java
@@ -2,7 +2,7 @@ import javax.swing.*;
 import java.awt.*;
 import java.util.Random;
 
-public class TerrainMap extends JPanel {
+class TerrainMap extends JPanel {
     private static final int UNDECLARED = 0;
     private static final int MOUNTAINS = 1;
     private static final int FOREST = 2;
@@ -15,25 +15,35 @@ public class TerrainMap extends JPanel {
     private int worldWidth, worldHeight;
     private int cellSize = 5;
     private Color[] colors = {
-            Color.BLACK, Color.GRAY, new Color(1, 107, 1),
-            Color.GREEN, Color.BLUE, new Color(0, 0, 170), Color.WHITE
+            Color.BLACK,                    // undeclared
+            Color.GRAY,                     // mountains
+            new Color(1, 107, 1),  // forest
+            Color.GREEN,                    // plains
+            Color.BLUE,                     // water
+            new Color(0, 0, 170),  // deep water
+            Color.WHITE                     // high mountains
     };
     private int types = 7;
+
+    // 1 if will cause a conflict 0 if won't cause a conflict
     private int[][] notAllowed = {
-            {0, 0, 0, 0, 0, 0, 0},
-            {0, 0, 0, 1, 1, 1, 0},
-            {0, 0, 0, 0, 1, 1, 1},
-            {0, 1, 0, 0, 0, 1, 1},
-            {0, 1, 1, 0, 0, 0, 1},
-            {0, 1, 1, 1, 0, 0, 1},
-            {0, 0, 1, 1, 1, 1, 0}
+            {0, 0, 0, 0, 0, 0, 0}, // undeclared can be next to anything
+            {0, 0, 0, 1, 1, 1, 0}, // mountains can't be next to plains, water, or deep water
+            {0, 0, 0, 0, 1, 1, 1}, // forest can't be next to water, deep water, or high mountains
+            {0, 1, 0, 0, 0, 1, 1}, // plains can't be next to mountains, deep water, or high mountains
+            {0, 1, 1, 0, 0, 0, 1}, // water can't be next to mountains, forest, or high mountains
+            {0, 1, 1, 1, 0, 0, 1}, // deep water can't be next to mountains, forest, plains, or high mountains
+            {0, 0, 1, 1, 1, 1, 0}  // high mountains can't be next to forest, plains, water, or deep water
     };
+    private boolean[][] fixedCells; // cells the user predetermines
 
     public TerrainMap(int width, int height) {
         this.worldWidth = width / cellSize;
         this.worldHeight = height / cellSize;
         this.map = new int[worldWidth][worldHeight];
+        this.fixedCells = new boolean[worldWidth][worldHeight];
 
+        // create empty map
         for (int x = 0; x < worldWidth; x++) {
             for (int y = 0; y < worldHeight; y++) {
                 map[x][y] = UNDECLARED;
@@ -41,16 +51,10 @@ public class TerrainMap extends JPanel {
         }
 
         setPreferredSize(new Dimension(width, height));
-        Timer timer = new Timer(100, e -> {
-            if (!leastConflicts()) {
-                repaint();
-            }
-        });
-        timer.start();
     }
 
     @Override
-    protected void paintComponent(Graphics g) {
+    public void paintComponent(Graphics g) {
         super.paintComponent(g);
         for (int x = 0; x < worldWidth; x++) {
             for (int y = 0; y < worldHeight; y++) {
@@ -60,46 +64,83 @@ public class TerrainMap extends JPanel {
         }
     }
 
+    public void startGeneration() {
+        Timer timer = new Timer(100, e -> {
+            if (!leastConflicts()) {
+                repaint();
+            }
+        });
+        timer.start();
+    }
+
+
+    public int getCellSize() {
+        return cellSize;
+    }
+
+    public void setCellType(int x, int y, int type) {
+        map[x][y] = type;
+        fixedCells[x][y] = true; // Mark this cell as fixed
+    }
+
+    /**
+     * gets rid of conflicts in the map
+     *
+     * @return true if no conflicts found and false if conflicts found
+     */
     private boolean leastConflicts() {
         boolean success = true;
-        int tries = 8;
+        int tries = 8; // probably try each terrain type once since 7 types
         Random rand = new Random();
 
+        // check random cells to avoid any biases in generation
         for (int i = 0; i < worldWidth * worldHeight; i++) {
             int x = rand.nextInt(worldWidth);
             int y = rand.nextInt(worldHeight);
-            int conflicts = checkConflicts(x, y, map[x][y]);
 
+            // skip fixed cells
+            if (fixedCells[x][y]) continue;
+
+            int conflicts = checkConflicts(x, y, map[x][y]);
             if (conflicts > 0 || map[x][y] == UNDECLARED) {
+                // try random terrain types and use the one that has the lesat conflicts
                 success = false;
                 int bestType = UNDECLARED;
-                int leastConflicts = Integer.MAX_VALUE;
+                int leastConflicts = 100; // large number
 
                 for (int j = 0; j < tries; j++) {
-                    int tempT = 1 + rand.nextInt(types - 1);
-                    int tempC = checkConflicts(x, y, tempT);
+                    int tempT = 1 + rand.nextInt(types - 1); // temporary random type
+                    int tempC = checkConflicts(x, y, tempT); // temporary num conflicts
 
                     if (tempC < leastConflicts) {
                         bestType = tempT;
                         leastConflicts = tempC;
                     }
                 }
-
-                map[x][y] = bestType;
+                map[x][y] = bestType; // fill in cell with type that has the least conflicts
             }
         }
-
         return success;
     }
 
+    /**
+     * checks for conflicts around the specified cell
+     * @param x x coordinate of cell
+     * @param y y coordinate of cell
+     * @param type terrain type of cell
+     * @return number of conflicts found around cell
+     */
     private int checkConflicts(int x, int y, int type) {
         int conflicts = 0;
-        int range = 3;
+        int range = 3; // how far away to check from cell if there is a conflict
 
+        // loop through
         for (int dx = -range; dx <= range; dx++) {
             for (int dy = -range; dy <= range; dy++) {
-                int tx = (dx + x + worldWidth) % worldWidth;
-                int ty = (dy + y + worldHeight) % worldHeight;
+                int tx = (dx + x + worldWidth) % worldWidth; // wrap around map if at edge
+                int ty = (dy + y + worldHeight) % worldHeight; // wrap around map if at edge
+
+                // checks table if position at tx,ty has conflicts with current type and adds a conflict if there is one
                 conflicts += notAllowed[type][map[tx][ty]];
             }
         }
